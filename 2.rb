92529e92b3b53dbb65fa10420bbac49583d2ea9a
Ret0052 <zik9357@mail.ru>
2022-09-13 13:05:49 +0300

    first commit

---
+++ 2.rb
@@ -0,0 +1,29 @@
+class Something
+    def doit
+        puts 256*2
+    end
+end
+
+=begin
+don`t care
+=end
+peremen = 1 + 23
+puts peremen
+CONST = 12
+puts CONST
+puts RUBY_VERSION
+puts CONST.object_id
+puts 24.between? 0,30
+puts 24.between? 0,20
+obj = Object.new
+puts obj.object_id
+something = Something.new
+puts something.class
+something.doit
+print 'something'
+pp 'something'
+pp something
+puts "String`s string"
+puts 'String\'s string'
+puts 3.respond_to? 'puts'
+puts 3.methods
---
+++ 2_task.rb
@@ -0,0 +1,12 @@
+class Users
+    def fio
+        puts 'AVD'
+    end
+    def prof
+        puts 'prof'
+    end
+end
+
+artem = Users.new
+artem.fio
+artem.prof
