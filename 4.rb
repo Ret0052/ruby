ad77a86e817f33a08accde76a911e43abac412c3
Ret0052 <zik9357@mail.ru>
2022-09-14 12:55:58 +0300

    initial commit

--- 4.rb
+++ 4.rb
@@ -23,3 +23,38 @@ str =<<~'here'
   2+2=#{2+2}
 here
 puts str
+str =`ifconfig`
+puts str
+str =%x(ifconfig)
+puts str
+str = '123456789'
+puts str[1]
+puts str[4,3]
+puts str[4..8]
+puts str[9..-1]
+puts str[-7..-2]
+str [4..7]='abc'
+puts 'Hello world'.sub('l','-')
+puts 'Hello world'.gsub('l','-')
+puts 0b1010101
+puts 0o755
+puts 0xffcc00
+puts 16763904.to_s(16)
+num=100.0
+inf= 100 / 0.0
+puts num.infinite?
+puts inf.infinite?
+puts num.nan?
+puts inf.nan?
+puts 2.0.to_i
+puts 2.to_f
+puts (1..5).first
+puts (1..5).last
+puts (1..5).include? 3
+puts (1..5).include? 6
+rgb = %w[краcный зеленый синий]
+rgb = %w[краcный\ зеленый\ синий] #c пробелами
+h={'f'=>'word','s'=>'hel'}
+puts h['s']
+h={f: 'word',s: 'hel'}
+puts h[:s]
---
+++ 4_task.rb
@@ -0,0 +1,32 @@
+#1
+pi='3.1415926535'
+puts pi[0..4]
+#2
+weeks =%w[пнд вт ср чт пт сб вск]
+puts weeks[0]
+puts weeks[1]
+puts weeks[2]
+puts weeks[3]
+puts weeks[4]
+puts weeks[5]
+puts weeks[6]
+#3
+col ={red: 'красный'}
+puts col[:red]
+#4
+puts (20..24)
+#5
+rgb =%w[red green blue]
+puts rgb[rand(0..2)]
+#6 я рот ебал этой формулы
+#7
+str = 'Hello'
+puts str.reverse
+#8
+str = '   Hello    '
+puts str.gsub(' ','')
+#9
+str ='Hello'
+str1 = 'Word'
+str = str + ' '+ str1
+puts str
