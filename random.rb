ccf2b7e878de02798a9a4b6fddde4860d083c117
Ret0052 <zik9357@mail.ru>
2022-12-06 17:26:22 +0300

    update random

--- random.rb
+++ random.rb
@@ -3,7 +3,7 @@ days=rand(1..3)
 raspisanie = []
 [0..days].each do |day|
     new_day = rand(0..4)
-    raspisanie << day unless raspisanie.include? new_day
+    raspisanie << new_day unless raspisanie.include? new_day
     redo if raspisanie.size < days
 end
 raspisanie.sort.each { |day| puts week[day] }
